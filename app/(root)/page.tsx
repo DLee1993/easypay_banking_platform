fd389dca7a84241b629bad29bd98b7b235541c69
David Lee <Dai180293@gmail.com>
2024-09-27 14:24:13 +0100

    added all components and content to root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,22 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
-  return (
-    <div>this is the home page</div>
-  )
+    const loggedIn = { firstname: "David" };
+
+    return (
+        <section className="home">
+            <section className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        user={loggedIn?.firstname || "Guest"}
+                        title="Welcome"
+                        subtext="Access and manage your account and transactions effectively"
+                    />
+                    <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={2475.52} />
+                </header>
+            </section>
+        </section>
+    );
 }
