9384428885b362a6b087fa8452e7a2070649f9e4
David Lee <Dai180293@gmail.com>
2024-10-06 10:17:46 +0100

    reduced animated counter duration

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -5,7 +5,7 @@ import CountUp from "react-countup";
 const Animatedcounter = ({ amount }: { amount: number }) => {
     return (
         <span className="w-full">
-            <CountUp start={0} end={amount} decimals={2} decimal="." separator="," prefix="£" duration={10} />
+            <CountUp start={0} end={amount} decimals={2} decimal="." separator="," prefix="£" duration={1} />
         </span>
     );
 };
